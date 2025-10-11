1f11d76f7f36dc84b11d89983e3e6b96f46d1272
Sonya Zhdanova <sonyazhdanovaaa@gmail.com>
2025-10-11 13:52:33 +0300

    3 zadanie

--- 1.py
+++ 1.py
@@ -4,3 +4,7 @@ print(squares)
 num = list(range(1, 21))
 ch_num = [x for x in num if x % 2 == 0]
 print(ch_num)
+
+words = ['python', 'java', 'c++', 'Rust', 'go']
+spisok = [x.upper() for x in words if len(x)>3]
+print(spisok)
