69f36e5b7252fb30ec7b53940587bc8e38b37efb
Sonya Zhdanova <sonyazhdanovaaa@gmail.com>
2025-10-11 14:35:34 +0300

    7 zadaniee

---
+++ 7.py
@@ -0,0 +1,14 @@
+from fractions import Fraction
+
+f1 = Fraction(3, 4)
+f2 = Fraction(5, 6)
+
+symma = f1 + f2
+vychitanie = f1 - f2
+proizvedenie = f1 * f2
+delenie = f1 / f2
+
+print(symma)
+print(vychitanie)
+print(proizvedenie)
+print(delenie)
